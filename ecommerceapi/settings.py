b1fe46bde0c4fa2bd0f97f785bd8b1f89480cdc2
ZiadTamer77 <ziademam7798@gmail.com>
2025-11-26 14:35:46 +0000

    First migration

--- ecommerceapi/settings.py
+++ ecommerceapi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -126,4 +127,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# AUTH_USER_MODEL = "core.User"
+AUTH_USER_MODEL = "core.User"
