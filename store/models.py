27c9a3a80f3dbce6ca258497e44e069f3442550d
ZiadTamer77 <ziademam7798@gmail.com>
2025-11-27 10:57:18 +0000

    created the customer model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user"
+    )
+
+    phone = models.CharField(max_length=20)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
