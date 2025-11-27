9005b9bada8639f8e071726fc51abbf3f724095c
ZiadTamer77 <ziademam7798@gmail.com>
2025-11-27 18:24:09 +0000

    added customer model to admin page

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["user"]
+    list_display = [
+        "user__first_name",
+        "user__last_name",
+        "user__email",
+        "phone",
+        "birth_date",
+    ]
+
+    list_per_page = 10
+
+    search_fields = [
+        "user__first_name__istartswith",
+        "user__last_name__istartswith",
+    ]
+
+    list_select_related = ["user"]
--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+
 class Customer(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user"
@@ -11,3 +12,6 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
+
+    class Meta:
+        ordering = ["user__first_name", "user__last_name"]
