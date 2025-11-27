f3c59b540b8f484cf4588e1b6f4c281e8dfc2492
ZiadTamer77 <ziademam7798@gmail.com>
2025-11-27 14:17:16 +0000

    added the user model to the admin page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "usable_password",
+                    "password1",
+                    "password2",
+                    "email",
+                    "first_name",
+                    "last_name",
+                ),
+            },
+        ),
+    )
